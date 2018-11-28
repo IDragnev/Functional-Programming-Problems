db6897bc343f20d664f73ee3e0565461adc18e48
IDragnev <iddragnev@gmail.com>
2018-11-28 22:54:27 +0200

    Add more functions for Lists in Haskell

--- Week 8/Lists.hs
+++ Week 8/Lists.hs
@@ -1,6 +1,6 @@
 module Lists where
  
-import Prelude hiding (head, tail, null, length, enumFromTo, enumFromThenTo, (++), reverse)
+import Prelude hiding (all, any, sorted, take, drop, map, filter, foldl, foldr, (!!), head, tail, null, length, enumFromTo, enumFromThenTo, (++), reverse)
 
 head :: [a] -> a
 head (h:_) = h
@@ -33,3 +33,41 @@ enumFromStepTo from step to
 reverse :: [a] -> [a]
 reverse [] = []
 reverse (h:t) = reverse t ++ [h]
+
+(!!) :: Int -> [a] -> a
+(!!) _ [] = error "Index out of range"
+(!!) 0 (h:_) = h
+(!!) n (_:t) = (!!) (n - 1) t
+
+map f [] = []
+map f (h : t) = f h : map f t
+
+filter p [] = []
+filter p (h:t) 
+ | p h = h : filter p t
+ | otherwise = filter p t
+
+foldl op result [] = result
+foldl op result (h : t) = foldl op (op result h) t 
+
+foldr op nv [] = nv
+foldr op nv (h : t) = op h (foldr op nv t)
+
+take :: Int -> [a] -> [a]
+take n [] = []
+take 0 _ = []
+take n (h : t) = h : (take (n - 1) t)
+
+drop :: Int -> [a] -> [a]
+drop _ [] = []
+drop 0 list = list
+drop n (h : t) = drop (n - 1) t
+
+sorted [] = True
+sorted [_] = True
+sorted (a : b : t) = a <= b && sorted (b : t)
+
+all p [] = True
+all p (h : t) = p h && all p t
+
+any p list = not (all (\ x -> not (p x)) list)
