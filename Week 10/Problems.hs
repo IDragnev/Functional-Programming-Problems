a134d876668c6bdaf95a4182efc6a82d13d4b729
IDragnev <iddragnev@gmail.com>
2018-12-16 23:07:13 +0200

    Add more solutions to Week 10

--- Week 10/Problems.hs
+++ Week 10/Problems.hs
@@ -24,3 +24,39 @@ scanr :: (a -> b -> b) -> b -> [a] -> [b]
 --scanr op nv (h:t) = (h `op` rh) : rest
 -- where rest@(rh:_) = scanr op nv t
 scanr op nv = foldr (\x rest@(rh:_) -> (x `op` rh) : rest) [nv]
+
+enumFromStep from step = from : enumFromStep (from + step) step
+
+sumProducts :: (Num c, Foldable t) => [t c] -> c
+sumProducts = sum . map product
+
+timesFound :: Eq t => t -> [t] -> Int
+timesFound _ [] = 0
+timesFound x (h:t) 
+ | x == h = 1 + timesInTail
+ | otherwise = timesInTail
+ where timesInTail = timesFound x t
+
+occurrences :: Eq t => [t] -> [t] -> [Int]
+occurrences lhs rhs = map (\x -> timesFound x rhs) lhs
+
+matchingLengths :: [[t]] -> Bool
+matchingLengths [] = True
+matchingLengths list = all (==h) lens
+ where lens@(h:_) = map length list
+   
+setInsert :: Ord t => t -> [t] -> [t]    
+setInsert x [] = [x]
+setInsert x set@(h:t) 
+ | x == h = set
+ | x > h = h:setInsert x t
+ | otherwise = x:set
+
+setUnion :: Ord t => [t] -> [t] -> [t]
+setUnion lhs rhs = foldr setInsert rhs lhs
+
+setIntersect :: Eq t => [t] -> [t] -> [t]
+setIntersect lhs rhs = filter ((flip elem) rhs) lhs
+
+setDiff :: Eq t => [t] -> [t] -> [t]
+setDiff lhs rhs = filter (\x -> not (elem x rhs)) lhs
