6245bb03fd1c0efbd803cfef8af74274d060f0c4
IDragnev <iddragnev@gmail.com>
2019-01-03 23:25:52 +0200

    Add implementation for NFAs

---
+++ NFAs/NFA.hs
@@ -0,0 +1,109 @@
+module NFA where
+
+type State = Int
+type Transition = (State, Char, State)
+
+data NFA = NFA { states :: [State]
+               , initialStates :: [State]
+	       , finalStates :: [State]
+	       , delta :: [Transition]
+	       }
+ deriving (Show, Read)
+
+epsilon :: NFA
+epsilon  = NFA { states = [0]
+               , initialStates = [0]
+	       , finalStates = [0]
+	       , delta = [] 
+	       }
+
+singleton :: Char -> NFA
+singleton symbol = NFA { states = [0, 1]
+                       , initialStates = [0]
+		       , finalStates = [1]
+		       , delta = [(0, symbol, 1)]
+		       }
+
+shiftList :: Int -> [Int] -> [Int]
+shiftList n = map (+n)
+
+shiftDelta :: Int -> [Transition] -> [Transition]
+shiftDelta n = map (\(from, with, to) -> (from + n, with, to + n)) 
+
+shiftNFA :: Int -> NFA -> NFA
+shiftNFA n (NFA states initial final delta) = 
+   NFA (shiftList n states)
+       (shiftList n initial)
+       (shiftList n final) 
+       (shiftDelta n delta)
+
+merge :: NFA -> NFA -> NFA
+merge (NFA states1 init1 final1 delta1) 
+      (NFA states2 init2 final2 delta2) = 
+         NFA (states1 ++ states2)
+	     (init1 ++ init2)
+	     (final1 ++ final2)
+	     (delta1 ++ delta2)
+
+union :: NFA -> NFA -> NFA
+union lhs rhs = merge lhs (shiftNFA n rhs)
+ where n = length (states lhs)
+
+genTransitions :: State -> Char -> [State] -> [Transition]
+genTransitions from with states = map (\q -> (from, with, q)) states
+
+mergingTransitions :: NFA -> NFA -> [Transition]
+mergingTransitions (NFA _ _ finalsLhs deltaLhs) (NFA _ initialsRhs _ _) = 
+  let transitionsToFinal = filter (\(_, _, q) -> elem q finalsLhs) deltaLhs
+      transitionsToInitial = map (\(from, with, to) -> genTransitions from with initialsRhs) transitionsToFinal 
+  in concat transitionsToInitial
+
+initialStatesOfConcatenation :: NFA -> NFA -> [State]
+initialStatesOfConcatenation (NFA _ initial final _) rhs
+ | any ((flip elem) final) initial = initial ++ (initialStates rhs)
+ | otherwise = initial
+
+concatenate :: NFA -> NFA -> NFA
+concatenate lhs@(NFA statesLhs initialLhs finalLhs deltaLhs) rhs = 
+   NFA newStates newInitials newFinals newDelta
+   where
+       newStates = statesLhs ++ (states shiftedRhs)
+       newInitials = initialStatesOfConcatenation lhs shiftedRhs
+       newFinals = finalStates shiftedRhs
+       newDelta = deltaLhs ++ (delta shiftedRhs) ++ (mergingTransitions lhs shiftedRhs)  
+       shiftedRhs = shiftNFA n rhs
+       n = length statesLhs
+
+plus :: NFA -> NFA
+plus nfa@(NFA states initials finals delta) = NFA states initials finals newDelta
+ where newDelta = delta ++ (mergingTransitions nfa nfa)
+
+star :: NFA -> NFA
+star nfa = union (plus nfa) epsilon
+
+matches :: String -> NFA -> Bool
+matches word nfa = any (\q -> canWalk word nfa q) (states nfa)
+
+canWalk :: String -> NFA -> State -> Bool
+canWalk [] (NFA _ _ finals _) q = elem q finals
+canWalk (h:t) nfa@(NFA states initials finals delta) q = 
+    any (\(q1, h, q2) -> q == q1 && canWalk t nfa q2) delta
+
+isLetter :: Char -> Bool
+isLetter x = elem x ['A'.. 'Z'] || elem x ['a'.. 'z']
+
+fromSymbol :: Char -> NFA
+fromSymbol _ = epsilon
+fromSynbol = singleton
+
+--fromRegex :: String -> NFA
+--fromRegex regex = buildNFA emptyNFA regex
+-- where
+--    buildNFA :: NFA -> String -> NFA
+--   buildNFA result [] = result
+--    buildNFA result (')':'+':t) = concatenate (plus result) (buildNFA emptyNFA t)
+--    buildNFA result (')':'*':t) = concatenate (star result) (buildNFA emptyNFA t)
+--    buildNFA result (x:'*':t) = concatenate $ (concatenate result (star $ fromSymbol x)) (buildNFA emptyNFA t)
+--    buildNFA result (x:'+':t) = concatenate $ (concatenate result (plus $ fromSymbol x)) (buildNFA emptyNFA t)
+--    buildNFA result (h:t) =  concatenate (fromSymbol h) (buildNFA emptyNFA t)
+--    emptyNFA = (NFA [] [] [] [])
